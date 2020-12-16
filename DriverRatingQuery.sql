6a9c7ba9b5148dc94affad0de5443e0f75addbbd
jackdenkin1374 <54447326+jackdenkin1374@users.noreply.github.com>
2020-12-15 19:01:57 -0500

    Add files via upload

---
+++ DriverRatingQuery.sql
@@ -0,0 +1,17 @@
+SELECT ps.Name as Driver_Name, ds.student_id AS Driver_ID, ds.rating as Average_Rating
+FROM (
+        SELECT p.person_name AS Name, s.student_id as student_id
+        FROM person AS p
+        INNER JOIN student AS s
+        ON p.person_id = s.person_id
+) as ps
+INNER JOIN (
+        SELECT AVG(dr.overallRating) AS rating, d.student_id AS student_id
+		FROM driver AS d
+        INNER JOIN driverrating AS dr
+        ON d.driver_id = dr.driver_id
+        GROUP BY dr.driver_id
+) AS ds
+ON ps.student_id = ds.student_id
+ORDER BY Average_Rating
+LIMIT 5
---
+++ RestaurantRatingQuery.sql
@@ -0,0 +1,7 @@
+EXPLAIN SELECT r.restaurant_name as Restaurant_Name, r.location as Location, AVG(rr.overallRating) as Average_Rating
+FROM restaurant AS r
+INNER JOIN restaurantrating AS rr
+ON r.restaurant_id = rr.restaurant_id
+GROUP BY rr.restaurant_id
+ORDER BY Average_Rating DESC
+LIMIT 5
