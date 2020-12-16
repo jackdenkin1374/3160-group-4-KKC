7e39cb2323a3cbdba209b84f4e4ff4c9155d710b
jackdenkin1374 <54447326+jackdenkin1374@users.noreply.github.com>
2020-12-15 19:06:33 -0500

    Update RestaurantRatingQuery.sql

--- RestaurantRatingQuery.sql
+++ RestaurantRatingQuery.sql
@@ -1,4 +1,4 @@
-EXPLAIN SELECT r.restaurant_name as Restaurant_Name, r.location as Location, AVG(rr.overallRating) as Average_Rating
+SELECT r.restaurant_name as Restaurant_Name, r.location as Location, AVG(rr.overallRating) as Average_Rating
 FROM restaurant AS r
 INNER JOIN restaurantrating AS rr
 ON r.restaurant_id = rr.restaurant_id
